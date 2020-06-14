b56ae85f6d96c679a601c66319632fa1130e0c67
Jan D <jandomalaon@gmail.com>
2020-06-14 16:22:02 -0400

    Started work on feature sliding animation.

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 $('.audience-btn').click(function() {
     $('#carousel-audience').carousel($(this).index());
 });
+
+// Detect scroll to slide in features
+$(window).scroll(function(e) {
+    var scroll = window.scrollTop();
+    if (scroll > 500) {
+        $("#first-feature").slide
+    }
+});
